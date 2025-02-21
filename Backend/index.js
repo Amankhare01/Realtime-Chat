b5a07a91947b6fcbfc97204e7133301a5318d845
Amankhare01 <amankhare.aa@gmail.com>
2025-02-21 12:46:50 +0530

    auth

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = 3000;
-// const MainRouter = require('./routes/index');
-
-// app.use('api/v1', MainRouter);
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Database connected Successfully")
+})
+.catch(err=> console.log("There is an error while connecting databse", err));
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 });
