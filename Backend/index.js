655a15af3dc283bb59d2d20c4b1d2559420ce436
Amankhare01 <amankhare.aa@gmail.com>
2025-02-22 23:58:11 +0530

    aut

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,6 @@ mongoose.connect(process.env.MONGO)
 .catch(err => console.error('MongoDB connection error:', err));
 
 
-// User Signup Route
 app.post("/users", async (req, res) => {
     try {
         const { email, password } = req.body;
