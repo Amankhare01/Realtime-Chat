0d3fbca56999fce1347a61a1be83eb5d11db82e8
Amankhare01 <amankhare.aa@gmail.com>
2025-03-12 12:39:12 +0530

    chore: remove commented-out signup function from auth routes

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -3,13 +3,6 @@ import { Login, Logout, Signup } from '../controller/auth.controller.js';
 
 const router = express.Router();
 
-// export const signup = async (req,res)={
-//     const{fullname, email, password}=req,body;
-
-//     try{
-//         if password.length
-//     }
-// }
 
 router.post('/login', Login)
 
