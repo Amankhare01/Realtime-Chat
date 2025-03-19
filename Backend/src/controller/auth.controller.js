e4d605c7e14996a070b7e9048d6d196933fcfed9
Amankhare01 <amankhare.aa@gmail.com>
2025-03-19 13:05:56 +0530

    feat: implement signup functionality with password hashing and JWT token generation

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -1,5 +1,43 @@
-export const Signup = (req,res)=>{
-    res.send('Signing Route');
+import { generateToken } from '../lib/utils';
+import User from '../models/user.model';
+import bcrypt from 'bcryptjs';
+
+export const Signup = async(req,res)=>{
+    const {fullname, email, password}=req.body;
+    try{
+        if (password.length <6){
+            return res.status(400).json({message: 'Password must contain atleast 6 characters'})
+        }
+        const user = await User.findOne({email});
+        if(user) return res.status(400).json({message: "User already exists"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password:hashedpassword
+        })
+
+        if (newUser){
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilepic:newUser.profilepic,
+            })
+        }else{
+            return res.status(400).json({message:"Invalid User Data"})
+        }
+    }
+    catch(err){
+        console.log("Internal Server Error",err),
+        res.status(500).json({message:"Internal server error"});
+    }
 }
 export const Login = (req,res)=>{
     res.send('Login Route');
