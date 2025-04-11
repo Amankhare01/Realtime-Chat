378e49c07aaf76da6bc065ac965650a3e811d005
Amankhare01 <amankhare.aa@gmail.com>
2025-04-11 17:23:50 +0530

    Update frontend file path and dependencies in package.json and package-lock.json

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
   app.get("*", (req,res)=>{
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "../frontend", "index.html"));
   })
 }
 console.log("Serving frontend from: ", path.join(__dirname, "../frontend/dist"));
