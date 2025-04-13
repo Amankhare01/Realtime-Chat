12dc226f4e90472b4cafbc051ceb4541106a540d
Amankhare01 <amankhare.aa@gmail.com>
2025-04-14 00:05:01 +0530

    Refactor static file serving logic and remove production environment check

--- backend/index.js
+++ backend/index.js
@@ -39,13 +39,12 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoute);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req,res)=>{
-      res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  })
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 // app.get("*", (req,res)=>{
 //   res.sendFile(path.join(__dirname, "https://realtimechatss.netlify.app"));
 // })
